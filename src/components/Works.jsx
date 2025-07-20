022f83012afa7a5e079fad38b7775e868cfd9998
DjRxxstr <dhyanj2016@gmail.com>
2025-07-20 11:05:45 +0530

    Minor styling changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -54,7 +54,7 @@ function ProjectCard(props) {
         <div className='flex flex-wrap justify-between items-center mt-4'>
           <div className='flex flex-wrap gap-0 hover:gap-6 transition-all duration-500 items-center'>
             {props.tags.map((tag) => (
-              <div key={tag} className='h-[40px] w-[40px] bg-black border-1 border-amber-50 p-1 rounded-full -ml-2'>
+              <div key={tag.name} className='h-[40px] w-[40px] bg-black border-1 border-amber-50 p-1 rounded-full -ml-2'>
                 <img
                   src={tag.icon} 
                   alt={tag.name}/>
@@ -69,8 +69,16 @@ function ProjectCard(props) {
             className='text-secondary hover:text-white underline decoration-2 decoration-secondary hover:decoration-white underline-offset-4 transition-all duration:300'>
             <div className='flex flex-wrap items-center'>
               <p className='mr-2'>Live Demo</p>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="16" 
+                height="16" 
+                fill="currentColor" 
+                className="bi bi-arrow-up-right" 
+                viewBox="0 0 16 16">
+                <path 
+                  fillRule="evenodd" 
+                  d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
               </svg>
             </div>
           </a>
